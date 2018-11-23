b72aecb6f6532d80e35a2cab51d89e31454c3cdd
Jakhongir Khusanov <worldj95@gmail.com>
2018-11-23 13:53:47 -0800

    implemented saved stations screen

--- app/screens/SavedScreen.js
+++ app/screens/SavedScreen.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { View, Text, Platform, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, Platform, ScrollView, StyleSheet, Dimensions } from 'react-native';
 import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import openMap from 'react-native-open-maps';
+
+const { width, height } = Dimensions.get('window');
 
 class SavedScreen extends React.Component {
   state = {
@@ -24,22 +28,93 @@ class SavedScreen extends React.Component {
       // marginTop: Platform.OS === 'android' ?  Expo.Constants.statusBarHeight : 0
     }
   });
+
+  gotoStation = (street, city, state, zip) => {
+    openMap({
+      travelType: ['drive'],
+      end: `${street}, ${city}, ${state}, ${zip}`
+    });
+  };
+  renderLikedJobs() {
+    return this.props.savedStations.map(station => {
+      const { id, name, status, location, street_address, city, state, zip_code } = station;
+      const initialRegion = {
+        latitude: location.coordinates[1],
+        longitude: location.coordinates[0],
+        latitudeDelta: 0.07,
+        longitudeDelta: 0.05
+      };
+      return (
+        <Card key={id} title={name} containerStyle={styles.cardStyle}>
+          <View style={{ height: height / 4 + 150 }}>
+            <MapView
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'ios'}
+              scrollEnabled={false}
+              initialRegion={initialRegion}
+              loadingEnabled={true}
+              liteMode={true}
+            >
+              <MapView.Marker key={station.id} coordinate={initialRegion} />
+            </MapView>
+            <View style={{ height: 130 }}>
+              <View style={styles.detailWrapper}>
+                <Text style={styles.stationStatus}>{status}</Text>
+              </View>
+              <Button
+                title='Directions'
+                buttonStyle={styles.directionsButton}
+                onPress={() => this.gotoStation(street_address, city, state, zip_code)}
+                titleStyle={styles.buttonTitle}
+              />
+            </View>
+          </View>
+        </Card>
+      );
+    });
+  }
   render() {
-    console.log(this.props.savedStations);
     return (
-      <View style={styles.container}>
-        <Text>Saved Stations Screen</Text>
+      <View>
+        <ScrollView>{this.renderLikedJobs()}</ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+  cardStyle: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  detailWrapper: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  italics: {
+    fontStyle: 'italic',
+    fontSize: 18
+  },
+  normal: {
+    fontStyle: 'normal',
+    fontSize: 16
+  },
+  directionsButton: {
+    backgroundColor: '#ff165d', // or 157EFB
+    width: 250,
+    height: height / 18 + 5,
+    borderColor: 'transparent',
+    borderWidth: 0,
+    borderRadius: 10
+  },
+  buttonTitle: {
+    fontWeight: '700',
+    color: 'white'
+  },
+  stationStatus: {
+    fontSize: 18,
+    color: '#0CE89C'
   }
 });
 
--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { clearSavedStations } from '../actions';
+import { Button } from 'react-native-elements';
 
 class SettingsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     headerTitle: 'Settings Screen'
   });
+  onButtonPress = () => {
+    Alert.alert(
+      'Warning',
+      'Are you sure you want to delete saved stations?',
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        {
+          text: 'Yes',
+          onPress: () => {
+            console.log('Yes Pressed'),
+              this.props.clearSavedStations(),
+              this.props.navigation.goBack();
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+  };
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text>Settings Screen where clear saved button will be located</Text>
+        <Button
+          title='Delete all saved stations'
+          titleStyle={{ fontWeight: '600' }}
+          icon={{ name: 'delete-forever', color: 'white' }}
+          buttonStyle={{
+            backgroundColor: '#F44336',
+            width: 300,
+            height: 45,
+            alignSelf: 'center'
+          }}
+          onPress={this.onButtonPress}
+        />
       </View>
     );
   }
 }
 
-export default SettingsScreen;
+export default connect(
+  null,
+  { clearSavedStations }
+)(SettingsScreen);
